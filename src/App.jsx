1e40c91b68e4b8d6e5e1f0bbc775fe300bca95a7
Justin <jdramos1@email.neit.edu>
2023-04-25 12:51:53 -0400

    submit custom fetch hook assignment

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+import FetchingData from './FetchingData';
+import PostingData from './PostingData';
+
+function App() {
+  const [showFetchData, setShowFetchData] = useState(true);
+  const [showPostData, setPostData] = useState(false);
+
+  const handleShowFetchDataDemo = () => {
+    setShowFetchData(true);
+    setPostData(false);
+  };
+
+  const handleShowPostDataDemo = () => {
+    setShowFetchData(false);
+    setPostData(true);
+  };
+
+  return (
+    <div id="pageContainer" style={{ display:'flex', width:"100%", justifyContent:'center'}}>
+      <div className="container">
+        <button onClick={() => handleShowFetchDataDemo()} className="button">
+          Fetching Data Demo
+        </button>
+        <button onClick={() => handleShowPostDataDemo()} className="button">
+          Posting Data Demo
+        </button>
+        <hr />
+        {showFetchData ? <FetchingData /> : <PostingData />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
